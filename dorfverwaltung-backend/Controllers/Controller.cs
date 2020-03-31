5332cd3d3bc726cb74636a9b20dc2d0af9822227
Morteza Jalilifar <morteza.jalilifar@publicispixelpark.de>
2020-03-31 23:08:06 +0200

    Add fake DB

---
+++ dorfverwaltung-backend/Controllers/Controller.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace dorfverwaltung_backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Controller : ControllerBase
+    {
+        static Controller() 
+        {
+            Dwarf.InitDwarfDB();
+        }
+
+        [HttpGet]
+        public List<Dwarf> Get()
+        {
+            return Dwarf.DwarfDB;
+        }
+    }
+}
--- dorfverwaltung-backend/classes/Dwarf.cs
+++ dorfverwaltung-backend/classes/Dwarf.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Collections.Generic;
 
 namespace dorfverwaltung_backend
 {
     public class Dwarf
     {
+        public static List<Dwarf> DwarfDB = new List<Dwarf>();
+
         public Dwarf(string name, int age, string gun, int powerFactor) 
         {
             this.Name = name;
@@ -16,5 +19,12 @@ namespace dorfverwaltung_backend
         public int Age { get; set; }
         public string Gun { get; set; }
         public int PowerFactor { get; set; }
+
+        public static void InitDwarfDB() 
+        {
+            DwarfDB.Add(new Dwarf("Gimli", 140, "[Axt, Schwert]", 27));
+            DwarfDB.Add(new Dwarf("Gumli", 163, "[Axt]", 17));
+            DwarfDB.Add(new Dwarf("Zwingli", 70, "[Zauberstab, Streithammer]", 60));
+        }
     }
 }
